87d99a3aa52336d7afff816aaea331661788397a
Meriem_Ben_Chaaben <meriembenchaaben97@gmail.com>
2019-04-04 18:06:05 +0100

    fixng routing doctor's entity

---
+++ src/main/webapp/app/entities/doctor/doctor.module.ts
@@ -0,0 +1,34 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { JhiLanguageService } from 'ng-jhipster';
+import { JhiLanguageHelper } from 'app/core';
+
+import { DoctorsPlatformSharedModule } from 'app/shared';
+import {
+    DoctorComponent,
+    DoctorDetailComponent,
+    DoctorUpdateComponent,
+    DoctorDeletePopupComponent,
+    DoctorDeleteDialogComponent,
+    doctorRoute,
+    doctorPopupRoute
+} from './';
+
+const ENTITY_STATES = [...doctorRoute, ...doctorPopupRoute];
+
+@NgModule({
+    imports: [DoctorsPlatformSharedModule, RouterModule.forChild(ENTITY_STATES)],
+    declarations: [DoctorComponent, DoctorDetailComponent, DoctorUpdateComponent, DoctorDeleteDialogComponent, DoctorDeletePopupComponent],
+    entryComponents: [DoctorComponent, DoctorUpdateComponent, DoctorDeleteDialogComponent, DoctorDeletePopupComponent],
+    providers: [{ provide: JhiLanguageService, useClass: JhiLanguageService }],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class DoctorsPlatformDoctorModule {
+    constructor(private languageService: JhiLanguageService, private languageHelper: JhiLanguageHelper) {
+        this.languageHelper.language.subscribe((languageKey: string) => {
+            if (languageKey !== undefined) {
+                this.languageService.changeLanguage(languageKey);
+            }
+        });
+    }
+}
